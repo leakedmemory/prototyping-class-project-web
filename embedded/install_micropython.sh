9678faceedb58dbf7eac5377b856211890954748
Lohan Yrvine <o.lohan.yrvine@gmail.com>
2024-04-11 12:15:49 -0300

    fix(embedded): typo

--- embedded/install_micropython.sh
+++ embedded/install_micropython.sh
@@ -4,12 +4,12 @@ FILE="micropython.uf2"
 
 if [ ! -f $FILE ]; then
     echo "MicroPython not found"
-    echo "Dowloading MicroPython..."
+    echo "Downloading MicroPython..."
     wget -c --show-progress --output-document=$FILE \
         https://micropython.org/resources/firmware/RPI_PICO_W-20240222-v1.22.2.uf2
+    echo "Download Complete"
 fi
 
 echo "Installing MicroPython..."
 cp micropython.uf2 /media/$USER/RPI-RP2
-
-echo "Installation Complete."
+echo "Installation Complete"
