54d69c90738c3fba73dc4d64fb03afc7d12b953d
Lohan Yrvine <o.lohan.yrvine@gmail.com>
2024-05-10 17:18:13 -0300

    feat(web): dashboard screen

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,18 +1,19 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import Login from "./screens/login/Login"
-import SignUp from "./screens/sign_up/SignUp"
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 
 import "./index.css"
+import Login from "./screens/login/Login"
+import SignUp from "./screens/sign_up/SignUp"
+import Dashboard from "./screens/dashboard/Dashboard";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Login />,
+    element: <Dashboard />,
   },
   {
     path: "/login",
---
+++ web/src/screens/dashboard/Dashboard.tsx
@@ -0,0 +1,49 @@
+import { PiUserCircleFill } from "react-icons/pi";
+import { MdOutlineAddCircle } from "react-icons/md";
+
+import "./dashboard.css"
+import PetCard, { petCardProps } from "../../components/pet_card/PetCard"
+import { useState } from "react";
+
+const userPets: petCardProps[] = [
+  {
+    name: "Sagwa",
+    type: "Cat",
+    age: 3,
+    breed: "Siamês",
+  },
+]
+
+export default function Dashboard() {
+  const [userProfile, _] = useState({
+    name: "Dudu",
+    email: "duduzinho@exemplo.com",
+    number: "(83) 90000-0001"
+  })
+
+  return (
+    <div className="dashboard">
+      <div className="profile-dashboard">
+        <PiUserCircleFill style={{ width: 100, height: 100 }} />
+        <div className="profile-data-dashboard">
+          <p>{userProfile.name}</p>
+          <p>{userProfile.email}</p>
+          <p>{userProfile.number}</p>
+        </div>
+      </div>
+      <div className="pets-header-dashboard">
+        <p style={{ fontSize: 20, fontWeight: "bold", lineHeight: 0 }}>
+          PETS ({userPets.length})
+        </p>
+        <MdOutlineAddCircle className="add-pet-dashboard" />
+      </div>
+      <div>
+        {userPets.map((data) => {
+          return (
+            <PetCard {...data} />
+          )
+        })}
+      </div>
+    </div>
+  )
+}
